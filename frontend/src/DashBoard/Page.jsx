81540d8b8743ee76061b833bf9bbc1404bddd860
Anupam Choudhary <87457759+Anupam0-0@users.noreply.github.com>
2025-05-01 09:44:30 +0530

    Update Page.jsx

--- frontend/src/DashBoard/Page.jsx
+++ frontend/src/DashBoard/Page.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Navbar from './components/Navbar'
-import Sidebar from './components/sidebar'
+import Sidebar from './components/Sidebar'
 import Content from './components/Content'
 
 const Page = () => {
