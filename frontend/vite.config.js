f9303b67e4ec9f25ffee16324fe49cba52d0a46b
Anupam0-0 <anupamchoudhary04@gmail.com>
2025-05-01 18:44:05 +0530

    vercel

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import { config } from "dotenv";
-config();
-
-// console.log("VITE_BASE_PATH:", import.meta.env.VITE_BASE_PATH); // Debugging log
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  // base: import.meta.env.VITE_BASE_PATH || "/fakturera-sow",
+  build: {
+    outDir: 'dist',
+  },
+  base: '/',
 });
 
